efcee2bcd225652b7781d56e8fa8600c5b8cf2d5
Joao <joaoglima@hotmail.com>
2022-08-22 18:07:57 -0300

    Exercicios de logica de programação III do programa start by capgemini

---
+++ src/main/java/Exercicio10.java
@@ -0,0 +1,18 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor do produto");
+        double preco = scan.nextDouble();
+        DecimalFormat dm = new DecimalFormat("###.##");
+
+        for (int i = 1; i <= 5; i++){
+            double precoDividido = preco / i;
+            System.out.println("O valor do produto em " + i + "x é de: " + dm.format(precoDividido));
+        }
+    }
+
+}
